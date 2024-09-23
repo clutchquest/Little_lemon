21acd0e5cbe6bd6b41bd578adbc6b591f67fbf56
clutchquest <closeinquires@yahoo.com>
2024-09-23 16:35:23 -0700

    added then deleted some stuff

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Homepage,
-    </div>
+    <nav className="App">
+      
+        <ul>
+          <li><a href="/home">Homepage</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reservations">Reservations</a></li>
+          <li><a href="/order online">Order Online</a></li>
+          <li><a href="/login">Login</a></li>  
+        </ul>
+        
+    </nav>
+
   );
 }
 
