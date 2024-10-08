3f31f45d63d0c9e536d60977304255ad3ee099ec
clutchquest <closeinquires@yahoo.com>
2024-10-08 15:28:29 -0700

    added new scripts for semantic componets. also added meta data, and tags for the webpage

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -3,19 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <nav className="App">
+    <div className="App">
       
-        <ul>
+        <header className="App-Navigation">
+       
+         <ul>
           <li><a href="/home">Homepage</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/menu">Menu</a></li>
           <li><a href="/reservations">Reservations</a></li>
           <li><a href="/order online">Order Online</a></li>
           <li><a href="/login">Login</a></li>  
-        </ul>
-        
-    </nav>
+        </ul> 
+        </header>
+     
+       <section></section>
 
+       <section></section>
+
+       <section></section>
+
+       <footer></footer>
+    </div>
+    
   );
 }
 
