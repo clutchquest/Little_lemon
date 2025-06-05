f610966896f251bd0a39b99ad7b024ba43548282
clutchquest <closeinquires@yahoo.com>
2025-06-05 12:25:07 -0700

    Short change 1

--- little-lemon/src/Footer.js
+++ little-lemon/src/Footer.js
@@ -1,7 +1,9 @@
+import logo from './Logo.svg';
 function Footer(){
     return(
         <footer className="App-footer">
             <h4>this is the footer</h4>
+            <img src={logo} className="App-logo" alt="logo" />
        </footer>
     )
 }
